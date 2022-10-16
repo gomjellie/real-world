425f90be1df6a0645eec9d04d78949e7d355de0e
gomjellie <gomjellie@gmail.com>
2022-10-16 16:57:50 +0900

    feat: login template

---
+++ src/pages/login.tsx
@@ -0,0 +1,60 @@
+import type { NextPage } from 'next'
+// import { signIn, signOut, useSession } from 'next-auth/react'
+import Head from 'next/head'
+
+const LoginPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Login - Real World</title>
+        <meta name="description" content="Login to Real World" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="auth-page">
+        <div className="container page">
+          <div className="row">
+            <div className="col-md-6 offset-md-3 col-xs-12">
+              <h1 className="text-xs-center">Sign up</h1>
+              <p className="text-xs-center">
+                <a href="">Have an account?</a>
+              </p>
+
+              <ul className="error-messages">
+                <li>That email is already taken</li>
+              </ul>
+
+              <form>
+                <fieldset className="form-group">
+                  <input
+                    className="form-control form-control-lg"
+                    type="text"
+                    placeholder="Your Name"
+                  />
+                </fieldset>
+                <fieldset className="form-group">
+                  <input
+                    className="form-control form-control-lg"
+                    type="text"
+                    placeholder="Email"
+                  />
+                </fieldset>
+                <fieldset className="form-group">
+                  <input
+                    className="form-control form-control-lg"
+                    type="password"
+                    placeholder="Password"
+                  />
+                </fieldset>
+                <button className="btn btn-lg btn-primary pull-xs-right">
+                  Sign up
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default LoginPage
